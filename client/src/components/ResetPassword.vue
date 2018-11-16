0a3ed0065022a332d190273ab6b1abd744b911d5
Brian Williams <williamsbm2000@comcast.net>
2018-11-15 17:52:55 -0800

    cleanup ResetPassword

--- client/src/components/ResetPassword.vue
+++ client/src/components/ResetPassword.vue
@@ -22,10 +22,10 @@ export default {
     return {
       email: "",
       rules: {
-        required: value => !!value || 'Required.',
+        required: value => !!value || "Required.",
         email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
+          const pattern = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+          return pattern.test(value) || "Invalid e-mail.";
         }
       }
     };
